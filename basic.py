9def3477a56d06cda448303ea4393e2fb8d14ea4
Guilherme Bezerra <gbsantos.it@gmail.com>
2018-09-18 12:35:10 -0300

    Scatter Plot

--- basic.py
+++ basic.py
@@ -7,6 +7,20 @@ np.random.seed(42)
 random_x = np.random.randint(1, 101, 100)
 random_y = np.random.randint(1, 101, 100)
 
-data = [go.Scattter(x=random_x, y=random_y, mode='markers')]
+data = [go.Scatter(x=random_x,
+                    y=random_y,
+                    mode='markers',
+                    marker=dict(
+                        size=12,
+                        color='rgb(51,204,153)',
+                        symbol='pentagon',
+                        line = {'width':2}
+                    ))]
 
-pyo.plot(data, filename='scatter.html')
+layout = go.Layout(title='Hello First Ploty',
+                   xaxis= {'title':'MY X AXIS'},
+                   yaxis=dict(title='MY Y AXIS'),
+                   hovermode='closest')
+
+fig = go.Figure(data=data, layout=layout)
+pyo.plot(fig, filename='scatter.html')
